de03388bb8bb2621b2b8ebc06b883278d3c7d13c
UlianaVladko <113168785+UlianaVladko@users.noreply.github.com>
2022-12-04 22:58:48 +0300

    Add files via upload

---
+++ Class/Class/Class.cpp
@@ -0,0 +1,38 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+class Weapon {
+public:
+    string name;
+    float damage;
+    float weight;
+
+    Weapon() {
+        name = "AWP";
+        damage = 115;
+        weight = 6;
+    };    
+    
+    Weapon(string name, float damage, float weight) {
+        this->name = name;
+        this->damage = damage;
+        this->weight = weight;
+    }
+};
+
+int main()
+{
+    setlocale(LC_ALL, "");
+
+   Weapon awp;
+   cout << "Name " << awp.name << "\t" << "Damage " << awp.damage << "\t" << "Weight " << awp.weight << endl;
+
+   Weapon* pawp = &awp;
+   pawp->name = "AK-47";
+   pawp->damage = 36;
+   pawp->weight = 4.79;
+   cout << "Name " << pawp->name << "\t" << "Damage " << pawp->damage << "\t" << "Weight " << pawp->weight << endl;
+}
+
